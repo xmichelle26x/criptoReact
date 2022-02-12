630db29ca715e4e15d03fc59d2d0fcbafcc3c53b
mangvi <mangulo@pichincha.com>
2022-02-12 16:59:18 -0500

    Formulario inicial

---
+++ criptos-react/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import styled from '@emotion/styled'
+import Formulario from './components/Formulario'
+import ImagenCripto from './img/imagen-criptos.png'
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px){
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #FFF;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 36px;
+
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 4px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`
+
+function App() {
+  return (
+    <Contenedor>
+      <Imagen
+        src={ImagenCripto}
+        alt='imagen de criptomonedas'
+      />
+      <div>
+        <Heading>Cotiza criptomonedas al instante</Heading>
+        <Formulario />
+      </div>    
+    </Contenedor>
+  )
+}
+
+export default App
